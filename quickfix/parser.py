ab9cc52f7f14602f955399e282f23cd233191ed5
Larcher <larcher@MacBook-Pro-de-Larcher.local>
2025-06-23 11:17:34 +0200

    parser ok postgres

--- quickfix/parser.py
+++ quickfix/parser.py
@@ -1,10 +1,12 @@
 import os
 import re
-#import psycopg2
+import psycopg2
 from datetime import datetime
+import json  # Add this import at the top
+
 
 # PostgreSQL connection params
-PG_HOST = os.getenv("PG_HOST", "postgres")
+PG_HOST = os.getenv("PG_HOST", "localhost")
 PG_PORT = int(os.getenv("PG_PORT", 5432))
 PG_DB = os.getenv("PG_DB", "fix_db")
 PG_USER = os.getenv("PG_USER", "admin")
@@ -33,22 +35,23 @@ def ensure_table_exists(cursor):
         );
     """)
 
-#def insert_fix_message(cursor, log_time, system, msg_type, raw_fix, fix_tags):
-#    cursor.execute("""
-#        INSERT INTO fix_messages (log_time, system, msg_type, raw_fix, fix_tags)
-#        VALUES (%s, %s, %s, %s, %s);
-#    """, (log_time, system, msg_type, raw_fix, fix_tags))
+
+def insert_fix_message(cursor, log_time, system, msg_type, raw_fix, fix_tags):
+    cursor.execute("""
+        INSERT INTO fix_messages (log_time, system, msg_type, raw_fix, fix_tags)
+        VALUES (%s, %s, %s, %s, %s);
+    """, (log_time, system, msg_type, raw_fix, json.dumps(fix_tags)))  # Convert fix_tags to JSON string
 
 def main():
-  #  conn = psycopg2.connect(
-  #      host=PG_HOST,
-  #      port=PG_PORT,
-  #      database=PG_DB,
-  #      user=PG_USER,
-  #      password=PG_PASSWORD
-  #  )
-  #  cursor = conn.cursor()
-  #  ensure_table_exists(cursor)
+    conn = psycopg2.connect(
+        host=PG_HOST,
+        port=PG_PORT,
+        database=PG_DB,
+        user=PG_USER,
+        password=PG_PASSWORD
+    )
+    cursor = conn.cursor()
+    ensure_table_exists(cursor)
 
     with open(FIX_LOG_FILE, "r") as f:
         for line in f:
@@ -61,12 +64,12 @@ def main():
             fix_raw = match.group("fix")
             fix_tags = parse_fix_message(fix_raw)
             msg_type = fix_tags.get("35", "UNKNOWN")
-
+            insert_fix_message(cursor, ts, system, msg_type, fix_raw, fix_tags)
             print(ts, system, msg_type, fix_raw, fix_tags)
 
- #   conn.commit()
- #   cursor.close()
- #   conn.close()
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 if __name__ == "__main__":
     main()
