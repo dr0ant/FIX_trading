d74e8a42b759085b31fc7f88c21a7215da31ff0d
antoine larcher <44400727+dr0ant@users.noreply.github.com>
2025-07-03 21:44:33 +0200

    model + db state

--- dagster/deployments/fix_pipeline/pictet_fix_project/models/01_staging_data/refine_fix_trading.sql
+++ dagster/deployments/fix_pipeline/pictet_fix_project/models/01_staging_data/refine_fix_trading.sql
@@ -5,8 +5,7 @@
 WITH enriched AS (
   SELECT
     *,
-    msg_timestamp - transact_time AS latency_interval
-    EXTRACT(EPOCH FROM msg_timestamp - sending_time) AS latency_seconds,
+    msg_timestamp - transact_time AS latency_interval,
     ROW_NUMBER() OVER (PARTITION BY cl_ord_id ORDER BY msg_timestamp) = 1 AS is_first_msg
   FROM {{ ref('stg_fix_trading') }}
 )
