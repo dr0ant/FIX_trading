4bbdcd8d089ae33396004c0b03ac90ceefef4256
Larcher <larcher@MacBook-Pro-de-Larcher.local>
2025-07-05 08:14:00 +0200

    new dbt docs

--- docs/compiled/pictet_fix_project/models/01_staging_data/dim_symbol.sql
+++
@@ -1,7 +0,0 @@
-
-
-select distinct
-  symbol,
-  currency
-from "duckdb_pictet"."main_staging_fix"."stg_fix_trading"
-where symbol is not null
--- docs/compiled/pictet_fix_project/models/01_staging_data/dim_system.sql
+++
@@ -1,6 +0,0 @@
-
-
-select distinct
-  sender_comp_id,
-  target_comp_id
-from "duckdb_pictet"."main_staging_fix"."stg_fix_trading"
